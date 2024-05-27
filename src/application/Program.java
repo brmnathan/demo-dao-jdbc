485c3fae04e697386da315be0bbdbb07178f8ae7
Marcos Nathan Moreno <marcosnathanpl@gmail.com>
2024-05-27 11:56:21 -0300

    Create class: Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dp = new Department("Books", 1);
+		Seller seller = new Seller("John", 1, "john@gmail.com", new Date(), 2000.0, dp);
+		
 		System.out.println(dp);
+		System.out.println(seller);
 
 	}
 
