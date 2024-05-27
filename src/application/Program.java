7efa76019ef374d64597b7e5276ece97c3862f07
Marcos Nathan Moreno <marcosnathanpl@gmail.com>
2024-05-27 11:46:24 -0300

    Create class: Department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dp = new Department("Books", 1);
+		System.out.println(dp);
+
+	}
+
+}
